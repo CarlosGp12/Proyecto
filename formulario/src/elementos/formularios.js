f1b75a66493e31557f954a6fa265329267a825ea
CarlosGp <carlos200012-@hotmail.com>
2022-04-06 17:58:17 -0500

    Proyecto git

---
+++ formulario/src/elementos/formularios.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const colores = {
+    borde: "#0075FF",
+    error: "#bb2929",
+    exito: "#1ed12d"
+}
+const Formulario = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px; 
+`;
