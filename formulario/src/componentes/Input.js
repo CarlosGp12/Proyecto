9b420a91149a3fb9cf2b4ef624f0a55a525fc5dd
CarlosGp12 <88398558+CarlosGp12@users.noreply.github.com>
2022-04-21 22:06:19 -0500

    Update Input.js

--- formulario/src/componentes/Input.js
+++ formulario/src/componentes/Input.js
@@ -1,65 +1,26 @@
 import React from "react";
-import {  LeyendaError, Input, Label } from "./../elementos/formularios";
+import { Input, Label } from "./../elementos/formularios";
 class InputText extends React.Component {
-    constructor(props) {
-      super(props);
-      this.actualizarState = this.actualizarState.bind(this);
-      this.state = {
-        value: "",
-        error: false,
-        mensajeError: ""
-      };
-    }
-  
-    actualizarState(e) {
-      const { name, value } = e.target;
-      console.log(this.props.validacion(value));
-  
-      if (this.props.validacion(value)) {
-        this.setState({
-          value,
-          error: false,
-          mensajeError: ""
-        });
-        this.props.actualizarState({
-          name, value, error: false
-        });
-      } else {
-        this.setState({
-          value,
-          error: true,
-          mensajeError: this.props.mensajeError
-        });
-        this.props.actualizarState({
-          name, value, error: true
-        });
-      }
-    }
-  
-  
-    render() {
-  
-      return (
-        <div>
-          <Label htmlFor={"id-" + this.props.name}>{this.props.label}</Label>
-          <Input
-            id={"id-" + this.props.name}
-            type="text"
-            name={this.props.name}
-            placeholder={this.props.placeholder}
-            onChange={this.actualizarState}
-          />
-          {
-            this.state.error ? (
-              <LeyendaError>{this.state.mensajeError}</LeyendaError>
-            ) : ("")
-          }
-        </div>
-      )
-    }
-  }
 
-  export default InputText;
+  render() {
+
+    return (
+      <div>
+        <Label htmlFor={"id-" + this.props.name}>{this.props.label}</Label>
+        <Input
+          id={"id-" + this.props.name}
+          type="text"
+          value={this.props.value}
+          name={this.props.name}
+          onChange={this.props.onChange}
+          placeholder={this.props.placeholder}
+        />
+      </div>
+    )
+  }
+}
+
+export default InputText;
 // import React from "react";
 // import {Label, GrupoInput, LeyendaError, IconoValidacion, Input} from './../elementos/formularios';
 // import { faCircleCheck, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
@@ -82,18 +43,18 @@ class InputText extends React.Component {
 //         <div>
 //             <Label htmlFor={name} valido={estado.valido} >{label}</Label>
 //             <GrupoInput>
-//                 <Input 
-//                     type={tipo} 
-//                     placeholder={placeholder} 
-//                     id={name} 
+//                 <Input
+//                     type={tipo}
+//                     placeholder={placeholder}
+//                     id={name}
 //                     value={estado.campo}
 //                     onChange={onChange}
 //                     onKeyUp={validacion}
 //                     onBlur={validacion}
 //                     valido={estado.valido}
 //                 />
-//                 <IconoValidacion 
-//                     icon={estado.valido=== 'true' ? faCircleCheck: faTimesCircle} 
+//                 <IconoValidacion
+//                     icon={estado.valido=== 'true' ? faCircleCheck: faTimesCircle}
 //                     valido={estado.valido}
 
 //                 />
